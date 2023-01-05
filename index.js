243d22d88c9482f2d4abd7de2b5fb19e49a489de
ganaga-puspa <ganaga-puspa>
2023-01-05 10:36:10 +0530

    one-many

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = gql`
-  type Query {
-    hello: [String]
-    products: [product!]
-  }
-  type product {
-    name: String!
-    description: String!
-    quantity: Int!
-    price: Float!
-    onSale: Boolean!
-  }
-`;
-
 const products = [
   {
     id: "53a0724c-a416-4cac-ae45-bfaedce1f147",
@@ -23,6 +9,7 @@ const products = [
     price: 42.44,
     image: "img-1",
     onSale: false,
+    catgoryID: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
   },
   {
     id: "c2af9adc-d0b8-4d44-871f-cef66f86f7f6",
@@ -32,6 +19,7 @@ const products = [
     price: 53.5,
     image: "img-2",
     onSale: false,
+    catgoryID: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
   },
   {
     id: "2c931e7e-510f-49e5-aed6-d6b44087e5a1",
@@ -41,6 +29,7 @@ const products = [
     price: 1.33,
     image: "img-3",
     onSale: true,
+    catgoryID: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
   },
   {
     id: "404daf2a-9b97-4b99-b9af-614d07f818d7",
@@ -50,6 +39,7 @@ const products = [
     price: 332,
     image: "img-4",
     onSale: false,
+    catgoryID: "34115aac-0ff5-4859-8f43-10e8db23602b",
   },
   {
     id: "6379c436-9fad-4b3f-a427-2d7241f5c1b1",
@@ -59,6 +49,7 @@ const products = [
     price: 23.11,
     image: "img-5",
     onSale: true,
+    catgoryID: "34115aac-0ff5-4859-8f43-10e8db23602b",
   },
   {
     id: "f01bcdec-6783-464e-8f9e-8416830f7569",
@@ -68,6 +59,7 @@ const products = [
     price: 59.99,
     image: "img-6",
     onSale: true,
+    catgoryID: "d914aec0-25b2-4103-9ed8-225d39018d1d",
   },
   {
     id: "a4824a31-5c83-42af-8c1b-6e2461aae1ef",
@@ -77,6 +69,7 @@ const products = [
     price: 427.44,
     image: "img-7",
     onSale: false,
+    catgoryID: "d914aec0-25b2-4103-9ed8-225d39018d1d",
   },
   {
     id: "b553085a-a7e0-4c9b-8a12-f971919c3683",
@@ -86,6 +79,7 @@ const products = [
     price: 77.0,
     image: "img-8",
     onSale: true,
+    catgoryID: "d914aec0-25b2-4103-9ed8-225d39018d1d",
   },
   {
     id: "47bf3941-9c8b-42c0-9c72-7f3985492a5b",
@@ -95,8 +89,48 @@ const products = [
     price: 93.44,
     image: "img-9",
     onSale: false,
+    catgoryID: "d914aec0-25b2-4103-9ed8-225d39018d1d",
   },
 ];
+
+const categories = [
+  {
+    id: "c01b1ff4-f894-4ef2-b27a-22aacc2fca70",
+    name: "Kitchen",
+  },
+  {
+    id: "34115aac-0ff5-4859-8f43-10e8db23602b",
+    name: "Garden",
+  },
+  {
+    id: "d914aec0-25b2-4103-9ed8-225d39018d1d",
+    name: "Sports",
+  },
+];
+
+const typeDefs = gql`
+  type Query {
+    hello: [String]
+    products: [Product!]
+    product(id: ID!): Product
+    categories: [Category!]
+    category(id: ID!): Category
+  }
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean!
+    image: String!
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+    products: [Product!]
+  }
+`;
 
 const resolvers = {
   Query: {
@@ -105,6 +139,28 @@ const resolvers = {
     },
     products: () => {
       return products;
+    },
+    product: (parent, args, context) => {
+      const PID = args.id;
+      const Product = products.find((pp) => pp.id === PID);
+      if (!Product) return null;
+      return Product;
+    },
+    categories: (parent, args, context) => {
+      return categories;
+    },
+    category: (parent, args, context) => {
+      const { id } = args;
+      console.log("categoryID", id);
+      return categories.find((cat) => cat.id === id);
+    },
+  },
+  Category: {
+    products: (parent, args, context) => {
+      const categoryId = parent.id;
+      console.log(":ss", categoryId);
+
+      return products.filter((prod) => prod.catgoryID === categoryId);
     },
   },
 };
